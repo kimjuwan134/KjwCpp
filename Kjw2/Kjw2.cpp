76ebfeee91925f9f66fe0c2b08c0e67574caff2d
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-09-21 19:47:28 +0900

    (2023.9.21) 템플릿

--- Kjw2/Kjw2.cpp
+++ Kjw2/Kjw2.cpp
@@ -1,109 +1,48 @@
 #include <iostream>
-#include <memory>
+#include "LinkedList.h"
 
 using namespace std;
 
 
-#pragma region RAII 패턴
+#pragma region 템플릿
 
-// 자원의 안전한 사용을 위해 객체가 쓰이는 범위를 벗어나게 되면
-// 자원을 해제해주는 기법.
+// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터 타입들을
+// 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수 있는 기능.
 
-class File
+template <typename T>
+void Add(T value1, T value2)
 {
-public : 
-	File()
-	{
-		cout << "Create File" << endl;
-	}
+	cout << value1 + value2 << endl;
+}
 
-	~File()
-	{
-		cout << "Delete File" << endl;
-	}
-};
+template <>
+void Add(char value1, char value2)
+{
+	cout << value1 << " : " << value2 << endl;
+}
+
+template<typename T1, typename T2>
+void Add(T1 value1, T2 value2)
+{
+	cout << value1 + value2 << endl;
+}
+
 
 #pragma endregion
 
-class Player
-{
-private :
-	int health;
-
-#pragma region weak 포인터
-
-	// 자신이 참조하고 있는 원시 포인터의 강한 참조 카운트가 0이 되면
-	// expired 라는 상태가 되는데, 이는 원시 포인터의 참조 카운트가
-	// 0이 되어 메모리가 해제되었으므로, 이 원시 포인터를 소유한
-	// weak 포인터를 유효하지 않다고 판단하여 해제하는 포인터.
-
-#pragma endregion
-
-	weak_ptr<Player> partner1;
-
-public :
-	Player()
-	{
-		cout << "Create Player" << endl;
-	}
-
-	~Player()
-	{
-		cout << "Delete Player" << endl;
-	}
-
-	void SetPartner(weak_ptr<Player> partner)
-	{
-		this->partner1 = partner;
-	}
-};
 
 int main()
 {
-#pragma region unique 포인터
+#pragma region 템플릿
 
-	// 단, 하나의 객체만 가리킬 수 있는 스마트 포인터.
-
-	//unique_ptr<File> uptr1(new File);
-
-	//cout << "uptr1의 값 : " << uptr1 << endl;
-
-	//unique_ptr<File> uptr2 = make_unique<File>();
-
-	//cout << "uptr2의 값 : " << uptr2 << endl;
-
-	//// 하나의 unique 포인터는 하나의 객체만 가질 수 있습니다.
-	//// 하지만 객체에 대한 소유권을 이전하는 것은 가능합니다.
-	//unique_ptr<File> uptr3 = move(uptr2);
-
-	//cout << "uptr3의 값 : " << uptr3 << endl;
-	//cout << "uptr2의 값 : " << uptr2 << endl;
+	//Add(10, 20);
+	//Add('A', 'B');
+	//Add(5.75f, 4.85f);
 
 #pragma endregion
 
-#pragma region shared 포인터
-
-	// 하나의 객체에 여러 개의 포인터가 공유할 수 있으며, 
-	// 공유할 때마다 참조 카운트를 이용해서 메모리를 관리하는
-	// 스마트 포인터.
-
-	//shared_ptr<File> sptr1(new File());
-	//cout << sptr1.use_count() << endl;
-
-	//shared_ptr<File> sptr2 = sptr1;
-	//cout << sptr1.use_count() << endl;
-
-	shared_ptr<Player> player1 = make_shared<Player>();
-	shared_ptr<Player> player2 = make_shared<Player>();
-
-	player1->SetPartner(player2);
-	player2->SetPartner(player1);
-
-
-
-#pragma endregion
-
-
+	LinkedList<int> linkedList;
+	linkedList.Push(10);
 
 
 	return 0;
---
+++ Kjw2/LinkedList.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+template <typename T>
+class LinkedList
+{
+private :
+	T data;
+
+public :
+	LinkedList() = default;
+
+	void Push(T data)
+	{
+		this->data = data;
+		std::cout << "push가 되어진 data의 값 : " << this->data << std::endl;
+	}
+
+};
+
+
